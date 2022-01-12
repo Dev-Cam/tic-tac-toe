8320cf6f4a0b3d02aef952503c7d7ee1c083f274
Cameron Reid <cameronreid@192-168-1-109.tpgi.com.au>
2022-01-12 13:01:21 +1100

    Working modal and reset on close

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 let $playerTurn = 1;
-let $playerOneWins = 0;
-let $playerTwoWins = 0;
+let $playerOneWins;
+let $playerTwoWins;
 let $player1Array = [];
 let $player2Array = [];
+let $player1Score = 0;
+let $player2Score = 0;
+
 
 const winConditions = [
     ["top1", "top2", "top3"],
@@ -22,11 +25,13 @@ const checkForWin = function(playerName, playerArr){
             for(elementOfChildArray of childArrayOfWinConditions){
                 if(playerArr.includes(elementOfChildArray)){
                     player1WinMoves ++;
-                    console.log("player1 winValue: ", player1WinMoves);
                 } // end of first if
             }  //end forloop
-            if( player1WinMoves === 3){
-                console.log("Hooray!");                
+            if( player1WinMoves >= 3){
+                $player1Score ++;
+                $playerOneWins.html($player1Score);
+                $("#winPlayer").html("Player 1 Wins!");
+                $("#winPopup").css("visibility", "visible");
             } //end of second if checking if winVal is 3
         } //end of winConditions for of  
     } else if (playerName === "player2"){
@@ -35,26 +40,34 @@ const checkForWin = function(playerName, playerArr){
             for(elementOfChildArray of childArrayOfWinConditions){
                 if(playerArr.includes(elementOfChildArray)){
                     player2WinMoves ++;
-                    console.log("player2 winValue: ", player2WinMoves);
                 } // end of first if
 
             } //end of forloop
-            if( player2WinMoves === 3){
-                console.log("Hooray!");
-                
+            if( player2WinMoves >= 3){
+                $player2Score ++
+                $playerTwoWins.html($player2Score);
+                $("#winPlayer").html("Player 2 Wins!");
+                $("#winPopup").css("visibility", "visible")
             } //end of second if checking if winVal is 
         } //end of winConditions for of  
     }// end else if
 }; //end of checkForWin
 
-//  google sub array forof loop
-
-
 $(function() {
-    
+    $playerOneWins = $("#p1Score");
+    $playerTwoWins = $("#p2Score");
+    const $closeButton = $("#exitModal")
     const $gameBox = $(".box");
-    
 
+    $closeButton.on("click", function(){
+        $("#winPopup").css("visibility", "hidden");
+        $gameBox.empty();
+        $player1Array = [];
+        $player2Array = [];
+        $playerTurn = 1;
+
+    })
+    
     $gameBox.on("click", function(event){
         if($playerTurn % 2 === 1){
             if($(this).has("div").length === 0){
@@ -63,10 +76,7 @@ $(function() {
                 $player1Array.push(event.target.id)
                 checkForWin("player1" ,$player1Array);
                 $playerTurn++
-            }  else {
-                alert("You can't go there!")
-        }
-            
+            }    
         } else {
             if($(this).has("div").length === 0){
             const $playerTwoMove =$("<div><img src='css/images/dry-clean.png')></div>"); 
@@ -74,12 +84,8 @@ $(function() {
                 $player2Array.push(event.target.id)
                 checkForWin("player2", $player2Array);
                 $playerTurn++
-            } else {
-                alert("You can't go there!")
-
-            }
-        } 
-        // end else
+            } 
+        } // end else
     }); //end gamebox onClick
 })
 
